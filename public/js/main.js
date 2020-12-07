8cff4bf2e7ba91ac53fd75fed317793a34ec7486
hovanvydut <hovanvydut@gmail.com>
2020-12-07 08:43:48 +0700

    add loading

--- public/js/main.js
+++ public/js/main.js
@@ -63,6 +63,8 @@ document
     .getElementById('trial-register-form-btn')
     .addEventListener('click', (event) => {
         event.preventDefault();
+        turnOnSpin();
+
         const email = document.getElementById('trial-register-form-input')
             .value;
         if (email == '') {
@@ -85,6 +87,7 @@ document
                     alert('Có lỗi xảy ra, vui lòng thử lại!');
                 }
 
+                turnOffSpin();
                 document.getElementById('notify-popup-close').click();
             });
     });
@@ -93,6 +96,7 @@ document
     .getElementById('contact-form-btn')
     .addEventListener('click', (event) => {
         event.preventDefault();
+        turnOnSpin();
         const email = document.getElementById('contact-form-email').value;
         const name = document.getElementById('contact-form-name').value;
         const message = document.getElementById('contact-form-message').value;
@@ -112,12 +116,43 @@ document
                 }),
             }).then((res) => {
                 if (res.status === 201) {
-                    alert('Bạn đã đăng kí dùng thử thành công!');
+                    alert(
+                        'Chúng tôi đã nhận được tin nhắn, sớm sẽ phản hồi với bạn'
+                    );
                     document.getElementById('contact-form-email').value = '';
                     document.getElementById('contact-form-name').value = '';
                     document.getElementById('contact-form-message').value = '';
                 } else {
                     alert('Có lỗi xảy ra, vui lòng thử lại!');
                 }
+                turnOffSpin();
             });
     });
+
+document.getElementById('notify-modal-1-btn').addEventListener('click', () => {
+    document
+        .getElementById('notify-modal-1')
+        .classList.add('notify-modal--hidden');
+});
+
+function turnOnModal1() {
+    document
+        .getElementById('notify-modal-1')
+        .classList.remove('notify-modal--hidden');
+}
+
+function turnOffModal1() {
+    document
+        .getElementById('notify-modal-1')
+        .classList.add('notify-modal--hidden');
+}
+
+function turnOnSpin() {
+    const elm = document.getElementsByClassName('cover-spin')[0];
+    elm.classList.add('cover-spin--show');
+}
+
+function turnOffSpin() {
+    const elm = document.getElementsByClassName('cover-spin')[0];
+    elm.classList.remove('cover-spin--show');
+}
